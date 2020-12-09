4b3e417a88f8b6e997620ee066e9e81a939b0004
dali888 <dali.burjaliani.1@btu.edu.ge>
2020-12-10 00:25:04 +0400

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Fragment/SettingsFragment.kt
@@ -0,0 +1,6 @@
+package com.example.myapplication.Fragment
+import  androidx.fragment.app.Fragment
+import com.example.myapplication.R
+
+class SettingsFragment: Fragment(R.layout.fragment_settings) {
+}
